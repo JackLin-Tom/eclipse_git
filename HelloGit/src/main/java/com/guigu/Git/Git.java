82a202c10a65f4fff27fff7f90a82633c456dd4d
justnowlin <2253805775@qq.com>
2022-02-28 19:41:46 +0800

    跟新3

--- HelloGit/src/main/java/com/guigu/Git/Git.java
+++ HelloGit/src/main/java/com/guigu/Git/Git.java
@@ -5,6 +5,7 @@ public class Git {
         System.out.println("Hello Git");
         System.out.println("跟新1");
         System.out.println("添加分支");
+        System.out.println("主干在第八行添加");
 
     }
 }
